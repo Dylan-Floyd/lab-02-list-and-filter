5e2f8aac432bb05f1a4dfcf1f062f11e40922719
Dylan Floyd <floyd800@gmail.com>
2021-09-21 13:46:47 -0700

    add working filter by keyword

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React from 'react';
 import './App.css';
 import images from './data.js';
 import ImageList from './ImageList.js';
 
-function App() {
-    return (
-        <div className="App">
-            <ImageList images={images} />
-        </div>
-    );
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: ''
+        }
+    }
 
-export default App;
+    handleSelectChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            keyword: e.target.value
+        })
+    }
+
+    render() {
+        const filteredImages = images.filter(image => !this.state.keyword || (this.state.keyword === image.keyword))
+        return (
+            <div className="App">
+                Select a Type:
+                <select onChange={ this.handleSelectChange }>
+                    <option value="">All</option>
+                    <option value="narwhal">narwhal</option>
+                    <option value="rhino">rhino</option>
+                </select>
+                <ImageList images={filteredImages} />
+            </div>
+        );
+    }
+}
