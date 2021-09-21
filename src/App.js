3325e8029f68631f22531c039060f3e3a41d079d
Dylan Floyd <floyd800@gmail.com>
2021-09-21 14:34:45 -0700

    add extra filtering

--- src/App.js
+++ src/App.js
@@ -7,28 +7,57 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            keyword: ''
+            type: '',
+            horns: -1,
+            hairy: 'any'
         }
     }
 
-    handleSelectChange = (e) => {
-        console.log(e.target.value);
+    handleTypeChange = (e) => {
         this.setState({
-            keyword: e.target.value
+            type: e.target.value
+        })
+    }
+
+    handleHornCountChange = (e) => {
+        this.setState({
+            horns: e.target.value === '' ? -1 : Number(e.target.value)
+        })
+    }
+
+    handleHairyChange = (e) => {
+        this.setState({
+            hairy: e.target.value === 'any' ? 'any' : Boolean(e.target.value) 
         })
     }
 
     render() {
         const uniqueKeywords = [...new Set(images.map(image => image.keyword))];
-        const filteredImages = images.filter(image => !this.state.keyword || (this.state.keyword === image.keyword))
+        const uniqueHornCounts = [...new Set(images.map(image => image.horns))];
         return (
             <div className="App">
+                <h2>Creatures With Horns</h2>
+
                 Select a Type:
-                <select onChange={ this.handleSelectChange }>
+                <select onChange={ this.handleTypeChange }>
                     <option value="">All</option>
                     { uniqueKeywords.map(keyword => <option value={keyword} key={keyword}>{keyword}</option>) }
                 </select>
-                <ImageList images={filteredImages} />
+
+                Select a Horn Count:
+                <select onChange={ this.handleHornCountChange }>
+                    <option value="">Any</option>
+                    { uniqueHornCounts.map(horns => <option value={horns} key={horns}>{horns}</option>) }
+                </select>
+
+                Do you prefer hairy creatures?
+                <select onChange={ this.handleHairyChange }>
+                    <option value="any">No Preference</option>
+                    <option value="true">Yes</option>
+                    <option value="">No</option>
+                </select>
+
+                <ImageList images={images} {...this.state}/>
             </div>
         );
     }
--- src/ImageList.js
+++ src/ImageList.js
@@ -3,9 +3,13 @@ import ImageItem from './ImageItem.js'
 
 export default class ImageList extends Component {
     render() {
+        const filteredImages = this.props.images
+            .filter(image => !this.props.type || (this.props.type === image.keyword))
+            .filter(image => this.props.horns < 0 || (this.props.horns === image.horns))
+            .filter(image => this.props.hairy === 'any' || (this.props.hairy === image.hairy));
         return (
             <div>
-                { this.props.images.map(image => <ImageItem {...image} key={image.title}/>) }
+                { filteredImages.map(image => <ImageItem {...image} key={image.title}/>) }
             </div>
         )
     }
